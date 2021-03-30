8fb5680a6a7aa7a6fb76a97d7642029ed7491228
deathangang <41196131+deathangang@users.noreply.github.com>
2021-03-30 22:53:29 +0900

    20.category user table test

--- study/src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ study/src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.example.study.repository;
 
 import com.example.study.StudyApplicationTests;
-import com.example.study.model.entity.Item;
 import com.example.study.model.entity.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -19,6 +18,30 @@ public class UserRepositoryTest extends StudyApplicationTests {
 
     @Test
     public void create(){
+        String account = "Test01";
+        String password = "Test01";
+        String status = "REGISTERED";
+        String email = "Test01@gmail.com";
+        String phoneNumber = "010-1111-2222";
+        LocalDateTime registeredAt = LocalDateTime.now();
+        LocalDateTime createdAt = LocalDateTime.now();
+        String createdBy = "AdminServer";
+
+        User user = new User();
+        user.setAccount(account);
+        user.setPassword(password);
+        user.setStatus(status);
+        user.setEmail(email);
+        user.setPhoneNumber(phoneNumber);
+        user.setRegisteredAt(registeredAt);
+        user.setCreatedAt(createdAt);
+        user.setCreatedBy(createdBy);
+
+        User newUser = userRepository.save(user);
+
+        Assertions.assertNotNull(newUser);
+
+        /*
         User user = new User();
         user.setAccount("TestUser03");
         user.setEmail("TestUser01@gmail.com");
@@ -28,23 +51,16 @@ public class UserRepositoryTest extends StudyApplicationTests {
 
         User newUser = userRepository.save(user);
         System.out.println("newUser: " +newUser);
+
+         */
     }
 
     @Test
     @Transactional
     public void read(){
-        Optional<User> user = userRepository.findByAccount("TestUser03");
+        User user = userRepository.findFirstByPhoneNumberOrderByIdDesc("010-1111-2222");
 
-        user.ifPresent(selectUser ->{
-
-            selectUser.getOrderDetailList().stream().forEach(detail ->{
-                        Item item = detail.getItem();
-                        System.out.println(detail.getItem());
-                    });
-            //System.out.println("user: " +selectUser);
-            //System.out.println("email: " +selectUser.getEmail());
-        });
-
+        Assertions.assertNotNull(user);
     }
 
     @Test

52738ae9a09670b67137d1e0bd2ce31451a98e82
deathangang <41196131+deathangang@users.noreply.github.com>
2021-04-12 09:10:56 +0900

    (homework)Creating Adimin page-1

--- study/src/main/java/com/example/study/controller/api/SettlementApiController.java
+++ study/src/main/java/com/example/study/controller/api/SettlementApiController.java
@@ -9,6 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/* SQL구문
+CREATE TABLE IF NOT EXISTS `study`.`settlement`
+(   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+ `price` DECIMAL(12,4) NULL DEFAULT NULL,
+ PRIMARY KEY (`id`))
+ ENGINE = InnoDB
+ DEFAULT CHARACTER SET = utf8mb4
+ COLLATE = utf8mb4_bin
+
+*/
+
+
 @RestController
 @RequestMapping("api/settlement")
 public class SettlementApiController extends CrudController<SettlementApiRequest, SettlementApiResponse, Settlement> {
